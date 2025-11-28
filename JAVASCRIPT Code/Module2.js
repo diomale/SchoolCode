e7da6148ab3a36af61c2f978a5436c2be32f766c
diomale <insane0225@gmail.com>
2025-11-28 21:01:23 +0800

    new commit

---
+++ JAVASCRIPT Code/Module2.js
@@ -0,0 +1,39 @@
+//Question 1
+let ticket = {
+  from: "Sindol",
+  to: "Olongapo",
+  price: 97
+};
+
+console.log(`Ticket from: ${ticket.from}`)
+console.log(`Ticket to: ${ticket.to}`)
+console.log(`Ticket price: ${ticket.price}`)
+
+//Question 2
+let person = {}
+person.name = "Diomale "
+person.surname = "Romero";
+
+let AllName = person.name + person.surname;
+console.log(AllName)
+
+//Question 3
+let book = [
+  {
+  title: "Speaking JavaScript" ,
+  author: "Axel Rauschmayer",
+  NoP: 460
+  },
+
+  {
+  title: "Programming JavaScript Applications" ,
+  author: "Eric Elliott",
+  NoP: 254
+  },
+
+  {
+  title: "Understanding ECMAScript 6" ,
+  author: "Nicholas C. Zakas",
+  NoP: 352
+  }
+]
