cbd6964d0197fcb3d8342d4aaa6060cbe638eed4
diomale <insane0225@gmail.com>
2025-11-29 13:01:39 +0800

    done

--- JAVASCRIPT Code/Module2.js
+++ JAVASCRIPT Code/Module2.js
@@ -15,10 +15,10 @@ person.name = "Diomale "
 person.surname = "Romero";
 
 let AllName = person.name + person.surname;
-console.log(AllName)
+console.log(`Made by: ${AllName}`)
 
 //Question 3
-let book = [
+let books = [
   {
   title: "Speaking JavaScript" ,
   author: "Axel Rauschmayer",
@@ -37,3 +37,20 @@ let book = [
   NoP: 352
   }
 ]
+//Question 4
+let new_book = {
+  title: "Learning JavaScript Design Patterns" ,
+  author: "Addy Osmani",
+  NoP: 252
+}
+
+books.push(new_book);
+
+console.log(books.length)
+console.log(books)
+console.log(`Book no.1 ${books[0].title}`)
+console.log(`Book no.2 ${books[1].title}`)
+console.log(`Book no.3 ${books[2].title}`)
+console.log(`Book no.4 ${books[3].title}`)
+
+
