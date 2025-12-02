7cdaa8f0791842b8dce1634a918645014f424732
diomale <insane0225@gmail.com>
2025-12-02 09:34:54 +0800

    new commit

---
+++ JAVASCRIPT Code/Module 3/practice.js
@@ -0,0 +1,5 @@
+let height = prompt("Enter your height:");
+let weight = prompt("Enter your weight:");
+let length = prompt("Enter your length:");
+let volume = height * weight * length;
+alert("The volume is: " + volume);
